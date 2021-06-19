84eb5ae9ef229eed4429a29375d3cb019fe7853a
北代櫂 <0jr0572.7264b3x@ezweb.ne.jp>
2021-06-19 12:33:09 +0900

    feature: login-logic

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: "1036557015693-fjhubclujbaf4sqp26ur81cmvottuhqu.apps.googleusercontent.com", 
+        scope: "email"
+      }).then(() => {
+        const auth = window.gapi.auth2.getAuthInstance();
+
+        this.setState({ isSignedIn: auth.isSignedIn.get() });
+      });
+    });
+  }
+
+  renderAuth() {
+    if (this.state.isSignedIn === null) {
+      return <div>i dont know your google account</div>
+    } else if (this.state.isSignedIn) {
+      return <div>login with google!!</div>
+    }
+  }
+
+  loginWithGoogle = () => {
+    window.gapi.auth2.getAuthInstance().signIn();
+  }
+
+  logoutFromGoogle = () => {
+    window.gapi.auth2.getAuthInstance().signOut();
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Magic Agent</h1>
+        {this.renderAuth()}
+        <div onClick={this.loginWithGoogle}>
+          ログイン
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
