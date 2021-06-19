2e5ea2b599a987246a357ccb1f39dbdace507ada
北代櫂 <0jr0572.7264b3x@ezweb.ne.jp>
2021-06-20 04:10:36 +0900

    feature: login/ui

--- src/component/GoogleAuthComponent.tsx
+++ src/component/GoogleAuthComponent.tsx
@@ -1,9 +1,40 @@
 import SocialButton from './SocialButton'
+import styled from 'styled-components'
+
+const Title = styled.div`
+  display: box;
+  color: #101841;
+  font-size: 3.7vw;
+  font-weight: 900;
+  text-align: center;
+  position: relative;
+  top: 20vw;
+  font-family: Roboto;
+  @media (max-width: 414px) {
+    font-size: 7vw;
+    top: 65vw;
+  }
+`
+
+const Back = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #4f85a68c;
+`
+
+const Body = styled.body`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`
 
 const GoogleAuthComponent = () => {
   // eslint-disable-next-line no-unused-vars
   const handleSocialLogin = (user: void) => {
     localStorage.setItem('login', 'true')
+    window.location.href = '/'
   }
 
   const handleSocialLoginFailure = (err: void) => {
@@ -12,16 +43,17 @@ const GoogleAuthComponent = () => {
   }
 
   return (
-    <div>
-      <SocialButton
-        provider="google"
-        appId="1036557015693-fjhubclujbaf4sqp26ur81cmvottuhqu.apps.googleusercontent.com"
-        onLoginSuccess={handleSocialLogin}
-        onLoginFailure={handleSocialLoginFailure}
-      >
-        Googleログイン
-      </SocialButton>
-    </div>
+    <>
+      <Back>
+        <Title>Magic Agent</Title>
+        <SocialButton
+          provider="google"
+          appId="1036557015693-fjhubclujbaf4sqp26ur81cmvottuhqu.apps.googleusercontent.com"
+          onLoginSuccess={handleSocialLogin}
+          onLoginFailure={handleSocialLoginFailure}
+        ></SocialButton>
+      </Back>
+    </>
   )
 }
 
--- src/component/SocialButton.js
+++ src/component/SocialButton.js
@@ -1,12 +1,35 @@
 import React from "react";
 import SocialLogin from "react-social-login";
+import styled from "styled-components";
+
+const SocialButtons = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 23vw;
+  width: 33vw;
+  height: 5vw;
+  border-radius: 4%;
+  background: url("/googlebutton.jpg") center;
+  background-size: cover;
+  cursor: pointer;
+  transition: opacity 0.3s;
+  :hover {
+    opacity: 0.7;
+  }
+  @media (max-width: 414px) {
+    width: 70vw;
+    height: 13vw;
+    margin-top: 75vw;
+  }
+`;
+
 
 class SocialButton extends React.Component {
   render() {
     return (
-      <button onClick={this.props.triggerLogin} {...this.props}>
+      <SocialButtons onClick={this.props.triggerLogin} {...this.props}>
         {this.props.children}
-      </button>
+      </SocialButtons>
     );
   }
 }
