b8ac3e4621597610bb72817148f402df8205d1f9
kototo522 <huyejiben@gmail.com>
2021-06-19 18:08:15 +0900

    update: レスポンシブ対応とデザインの修正

--- src/component/my-page.tsx
+++ src/component/my-page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import BorderColorIcon from '@material-ui/icons/BorderColor'
 import AddIcon from '@material-ui/icons/Add'
 import Redbull from '../items/RedBull'
 import Zone from '../items/Zone'
@@ -8,66 +7,50 @@ import MonsterEnegy from '../items/MonsterEnegy'
 import CheletedLemon from '../items/ChelatedLemon'
 
 const Body = styled.body`
-  position: absolute;
+  position: relative;
   top: 0px;
   left: 0px;
   background-color: rgba(121, 167, 217, 0.15);
-  min-height: 100vh;
+  height: 100vh;
   width: 100vw;
+  margin-left: 0px;
 `
 
-const UserName = styled.text`
-  position: absolute;
-  top: 140px;
-  left: 50vw; /*親要素を起点に左から50%*/
-  transform: translateY(-50%) translateX(-50%); /*要素の大きさの半分ずつを戻す*/
-  -webkit-transform: translateY(-50%) translateX(-50%);
-  height: 40px;
-  width: 600px;
-  margin: auto;
+const UserName = styled.p`
   text-align: center;
   font-size: 40px;
+  margin: 0;
+  padding-top: 60px;
   font-family: 'M PLUS Rounded 1c';
   color: #101841;
 `
 
-const RewriteButton = styled(BorderColorIcon)`
-  position: absolute;
-  color: #101841;
-  top: 110px;
-  right: 0px;
-  margin-right: 40vw;
-`
-
 const AddButton = styled(AddIcon)`
-  position: absolute;
+  margin-left: 80vw;
   color: #101841;
-  top: 90px;
-  right: 0px;
-  margin-right: 15vw;
 `
 
-const Since = styled.text`
-  position: absolute;
-  top: 185px;
-  right: 25vw; /*親要素を起点に左から50%*/
-  transform: translateY(-50%) translateX(-50%); /*要素の大きさの半分ずつを戻す*/
-  -webkit-transform: translateY(-50%) translateX(-50%);
-  height: 20px;
-  width: 200px;
-  margin: auto;
+const Since = styled.p`
   text-align: center;
-  font-size: 10px;
-  color: #101841;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  right: 0;
+  color: #555555;
 `
+
+// const Container = styled.div`
+//   display: flex;
+// `
+
 const MyPage = () => {
   return (
     <Body>
       <UserName>ユーザー名</UserName>
       <Since>2020年３月から社畜</Since>
-
-      <RewriteButton style={{ fontSize: 25 }} />
       <AddButton style={{ fontSize: 45 }} />
+      {/* <Container>
+
+      </Container> */}
     </Body>
   )
 }
