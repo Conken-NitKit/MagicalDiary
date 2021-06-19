8321f0edaec77ab407bdd35810ea9ea868b26d82
平田海輝 <seaglory2004@gmail.com>
2021-06-19 18:19:50 +0900

    add: 画像投稿用モーダルの大枠を追加しました。

---
+++ src/components/PostModal.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  background: rgba(0, 0, 0, 0.5);
+`
+
+export const PostModal = () => {
+  return <Container></Container>
+}
+
+export default PostModal
